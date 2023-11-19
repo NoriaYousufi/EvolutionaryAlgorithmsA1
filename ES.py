d40f93b59521a2b9e4b9dd06187c81ccc38ebcb6
Noria Yousufi <noriay2a@gmail.com>
2023-11-19 16:20:54 +0100

    created files

---
+++ ES.py
@@ -0,0 +1,16 @@
+"""
+Requirements
+- Implement a genetic algorithm solving the F18 and F19 problems
+- Implement an evolution strategy solving the F18 and F19 problems
+- Submit at least two files 'studentnumber1_studentnumber2_GA.py' and 'studentnumber1_studentnumber2_ES.py'.
+- Additional files of other functions are allowed. Please make sure we can get results by running 'python *_GA.py' and 'python *_ES.py' without additional arguments.
+- Submit a report introducing your algorithms and presenting the experimental results. 
+"""
+
+"""
+Task 2: Evolution Strategy
+- How to handle the representation of binary/discrete variables while applying evolution strategies?
+- What encoding and decoding methods will you use?
+- Which variators (e.g., one-� mutation, correlated mutation, discrete recombination, etc) and selectionmoperators will you use?
+- What’s your suggestion for the parameter settings (e.g., population size, step size, etc.)?
+"""
---
+++ GA.py
@@ -0,0 +1,22 @@
+"""
+Requirements
+- Implement a genetic algorithm solving the F18 and F19 problems
+- Implement an evolution strategy solving the F18 and F19 problems
+- Submit at least two files 'studentnumber1_studentnumber2_GA.py' and 'studentnumber1_studentnumber2_ES.py'.
+- Additional files of other functions are allowed. Please make sure we can get results by running 'python *_GA.py' and 'python *_ES.py' without additional arguments.
+- Submit a report introducing your algorithms and presenting the experimental results. 
+"""
+
+"""
+Task 1: Genetic Algorithm
+Some hints:
+- Which variators (i.e., mutation and crossover) and selection operators will you use?
+- What’s your suggestion for the parameter settings (e.g., population size, mutation rate, etc.)?
+"""
+
+import random
+import shutil
+from typing import List
+
+import ioh
+
