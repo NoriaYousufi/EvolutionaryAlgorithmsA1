695c1cadf8d701f5f1a3bdbfdd549e09995bfafe
JensVanBijs <jensvanbijsterveld@gmail.com>
2023-12-04 18:01:50 +0100

    Cleanup

--- ES.py
+++ ES.py
@@ -1,18 +1,3 @@
-"""
-Requirements
-- Implement a genetic algorithm solving the F18 and F19 problems
-- Implement an evolution strategy solving the F18 and F19 problems
-- Submit at least two files 'studentnumber1_studentnumber2_GA.py' and 'studentnumber1_studentnumber2_ES.py'.
-- Additional files of other functions are allowed. Please make sure we can get results by running 'python *_GA.py' and 'python *_ES.py' without additional arguments.
-- Submit a report introducing your algorithms and presenting the experimental results. 
-
-Task 2: Evolution Strategy
-- How to handle the representation of binary/discrete variables while applying evolution strategies?
-- What encoding and decoding methods will you use?
-- Which variators (e.g., one-� mutation, correlated mutation, discrete recombination, etc) and selectionmoperators will you use?
-- What’s your suggestion for the parameter settings (e.g., population size, step size, etc.)?
-"""
-
 import numpy as np
 from ioh import get_problem, logger, ProblemClass
 
@@ -115,9 +100,9 @@ def create_problem(fid: int):
 
     problem = get_problem(fid, dimension = DIMENSION, instance=1, problem_class=ProblemClass.PBO)
     l = logger.Analyzer(
-        root="data",  # the working directory in which a folder named `folder_name` (the next argument) will be created to store data
-        folder_name="run",  # the folder name to which the raw performance data will be stored
-        algorithm_name="genetic_algorithm",  # name of your algorithm
+        root="data",
+        folder_name="run",
+        algorithm_name="genetic_algorithm",
         algorithm_info="Practical assignment of the EA course",
     )
     problem.attach_logger(l)
